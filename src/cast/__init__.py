d504ecbcf1772d3faafc49df51fcb6f6cc790739
Sooraj <sooraj.crafts@gmail.com>
2026-03-21 13:26:07 +0800

    Update __init__.py

--- src/cast/__init__.py
+++ src/cast/__init__.py
@@ -1,1 +1,16 @@
+from .interceptor import patch_all
+from .recorder import record, capture_step, get_active_run
+from .models import Run, Step, ToolCall, RunStatus, StepType
 
+patch_all()
+
+__all__ = [
+    "record",
+    "capture_step",
+    "get_active_run",
+    "Run",
+    "Step",
+    "ToolCall",
+    "RunStatus",
+    "StepType",
+]
